1a6d7acbf8942e753cab72a5fa2166a9b607af20
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-07 10:11:38 +0000

    Section 3 - Hello React : Built it (Visibility Toggle)

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,32 +1,19 @@
 'use strict';
 
-console.log('App.js is running! Rolling ... Rolling ...');
+console.log('Running the BuildItApp ...');
 
 var appRoot = document.getElementById('app');
 
-// JSX - JavaScript XML
 var app = {
-    title: "Holidays",
-    subTitle: "Holidays Plans",
-    options: []
+    visibility: false,
+    details: "Here are some details"
 };
 
-var onFormSubmit = function onFormSubmit(event) {
-    event.preventDefault();
-
-    var option = event.target.elements.option.value;
-    if (option) {
-        app.options.push(option);
-        event.target.elements.option.value = "";
-        renderApp();
-    }
-    console.log("Form submitted");
-};
-
-var removeAll = function removeAll() {
-    app.options = [];
+var toggleVisibility = function toggleVisibility() {
+    app.visibility = !app.visibility;
     renderApp();
 };
+
 var renderApp = function renderApp() {
 
     var template = React.createElement(
@@ -35,111 +22,25 @@ var renderApp = function renderApp() {
         React.createElement(
             'h1',
             null,
-            app.title
+            'Visibility Toggle'
         ),
-        app.title && React.createElement(
-            'p',
-            null,
-            app.subTitle
-        ),
-        app.options.length > 0 ? "Here are the possible destination" : "No destinations",
         React.createElement(
             'button',
-            { onClick: removeAll },
-            'Remove all'
+            { onClick: toggleVisibility },
+            app.visibility ? "Hide details" : "Show details"
         ),
-        app.options.length > 0 && React.createElement(
-            'ol',
+        app.visibility && React.createElement(
+            'div',
             null,
-            app.options.map(function (option) {
-                return React.createElement(
-                    'li',
-                    null,
-                    option
-                );
-            })
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
             React.createElement(
-                'button',
+                'p',
                 null,
-                'Add destination'
+                app.details
             )
         )
     );
+
     ReactDOM.render(template, appRoot);
 };
 
 renderApp();
-
-/* // 1st Assignement : Create a new JSX template and render in the place of the first one
-const user = {
-    name: "HuLK",
-    age: 105,
-    location: "Banka"
-};
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
-    } else {
-        return undefined;
-    }
-}
-
-const userName = "HuLK";
-const userAge = 104;
-const userLocation = "Bagondack";
-const templateTow = (
-    <div>
-        <h1>{user.name ? user.name : "Anonymous"}</h1>
-        {(user.age && user.age >= 50) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
-
-
-
-const appRoot = document.getElementById('app');
-
-ReactDOM.render(templateTow, appRoot); */
-
-/* let count = 0;
-const addOne = () => {
-    count++;
-    console.log('addOne');
-    renderCounterApp();
-};
-const minusOne = () => {
-    count--
-    console.log('minusOne');
-    renderCounterApp();
-}
-const reset = () => {
-    count = 0;
-    console.log('reset');
-    renderCounterApp();
-}
-
-const renderCounterApp = () => {
-
-    const templateThree = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-
-    // console.log(templateThree);
-
-    const appRoot = document.getElementById('app');
-
-    ReactDOM.render(templateThree, appRoot);
-
-}
-
-renderCounterApp(); */
