3a4e687fd484bbcdbcd0606bf06c45b58b8eabde
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-09 12:39:55 +0000

    Section 4: React Components - 52. Default Exports

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,4 +1,4 @@
-import {isAdult, canDrink} from './person'
+import isSenior, {isAdult, canDrink} from './person'
 
 console.log(20, " is adult:", isAdult(20));
 console.log(18, " is adult:", isAdult(18));
@@ -6,3 +6,6 @@ console.log(17, " is adult:", isAdult(17));
 console.log(18, " can drink", canDrink(18));
 console.log(21, " can drink", canDrink(21));
 console.log(24, " can drink", canDrink(24));
+console.log(24, " is senior", isSenior(24));
+console.log(40, " is senior", isSenior(40));
+console.log(60, " is senior", isSenior(60));
