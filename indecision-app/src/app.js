c51f8e5f3f7dc511fbaec2689d920c0b880af785
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-09 12:28:09 +0000

    Section 4: React Components - 51. ES6 import/export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,8 @@
-console.log("tested");
+import {isAdult, canDrink} from './person'
+
+console.log(20, " is adult:", isAdult(20));
+console.log(18, " is adult:", isAdult(18));
+console.log(17, " is adult:", isAdult(17));
+console.log(18, " can drink", canDrink(18));
+console.log(21, " can drink", canDrink(21));
+console.log(24, " can drink", canDrink(24));
