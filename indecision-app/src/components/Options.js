d4521896fad290cff3c8a4e7f29010fd911535c3
hugues Kametcha <hugues.leunsa+atlassian@gmail.com>
2019-10-10 03:22:37 +0200

    Section 8: Styling React - 69. Styling the options list

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -3,14 +3,18 @@ import Option from './Option';
 
 const Options = (props) =>  (
     <div>
-        <button 
-            className="button button--link"
-            onClick={props.handleDeleteOptions}
-        >
-            Remove all
-        </button>
-        <p>{`Number of options: ${props.options.length}`}</p>
-        {props.options.length===0 && (<p>Please enter some options to get started!</p>)}
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your options</h3>
+            <button 
+                className="button button--link"
+                onClick={props.handleDeleteOptions}
+            >
+                Remove all
+            </button>
+        </div>
+        {props.options.length===0 && (
+            <p className="widget__message"> Please enter some options to get started!</p>
+        )}
         
         <ol>
         {props.options.map(option => <Option Optiontext={option} key={option} handleDeleteOption={props.handleDeleteOption}/>)}
