beef6db87706cf68b79a36f3047bd7d8a5e782d9
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-10 15:25:03 +0000

    Section 9: React-Router -81 Query Strings and URL Parameters

--- expensify-app/src/components/Headers.js
+++ expensify-app/src/components/Headers.js
@@ -6,7 +6,6 @@ const Headers = () => (
         <h1>Expensify</h1>
         <h2><NavLink to="/" activeClassName="is-active" exact={true} >Home</NavLink></h2>
         <h2><NavLink to="/create" activeClassName="is-active">Create</NavLink></h2>
-        <h2><NavLink to="/edit" activeClassName="is-active">Edit</NavLink></h2>
         <h2><NavLink to="/help" activeClassName="is-active">Help</NavLink></h2>
     </header>
 );
