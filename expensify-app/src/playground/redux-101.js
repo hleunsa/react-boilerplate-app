a3e9d26536ad1da5bd13cd70cbb528875935aeee
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-11 16:31:36 +0000

    Section 10: Redux - 90 Refactoring and Organizing

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,11 +1,31 @@
 import { createStore } from 'redux';
 
+// Action generators - functions that return action objects
+
+const incrementCount = ({incrementBy = 1} = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({decrementBy = 1} = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({count}) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
 const store = createStore((state = {count:0}, action)=> {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number'? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
             const decrementBy = typeof action.decrementBy === 'number'? action.decrementBy : 1;
@@ -30,31 +50,16 @@ const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5}));
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 3
-});
+store.dispatch(decrementCount({decrementBy: 3}));
 
-store.dispatch({
-    type: 'DECREMENT'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(setCount({ count: 101 }));
 
 unsubscribe();
 
-store.dispatch({
-    type: 'RESET'
-});
+store.dispatch(resetCount());
