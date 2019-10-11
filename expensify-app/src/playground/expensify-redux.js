c9083b92b056a5e45d5400090b9fb2b023cb2105
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-11 17:22:41 +0000

    Section 10: Redux - 92 Working with Multiple Reducers

---
+++ expensify-app/src/playground/expensify-redux.js
@@ -0,0 +1,64 @@
+import { createStore, combineReducers } from 'redux';
+
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+const filtersReducerDefaultState =  {
+    text: '',
+    sortBy: 'date', // date or amount
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+// Store creation
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
+
+const demoState = {
+    expenses: [
+        {
+            id: 'iddsdQDQEDFD',
+            description: 'October Rent',
+            note: 'This was the final payment for that adress',
+            amount: 1200,
+            createdAt: 0
+        }
+    ],
+
+    filters: {
+        text: 'rent',
+        sortBy: 'amount', // date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
+};
