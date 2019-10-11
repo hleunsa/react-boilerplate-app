cc0522766b65c02e293353fd3d616a32e275b61a
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-11 16:38:25 +0000

    Section 10: Redux - 91 Reducers

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -21,7 +21,11 @@ const resetCount = () => ({
     type: 'RESET'
 });
 
-const store = createStore((state = {count:0}, action)=> {
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
+
+const countReducer = (state = {count:0}, action)=> {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -44,7 +48,9 @@ const store = createStore((state = {count:0}, action)=> {
             return state;
     }
     return state;
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
