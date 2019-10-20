159caad4355548f3ea10c4b155de808c15e15107
Hugues Leunsa Kametcha <hlk@activeviam.com>
2019-10-20 23:12:26 +0000

    Section 12: Testing your App - 122. Testing user interaction

--- expensify-app/src/tests/components/ExpenseForm.test.js
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,46 @@ test('should render ExpenseForm with expense data', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should render error for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error')).toBe('Please enter a description and an Amount for your expense!');
+});
+
+test('should set description on input change', () => {
+    const value = 'New description';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('description')).toBe(value);
+});
+
+test('should set description on textarea change', () => {
+    const value = 'New note value';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('note')).toBe(value);
+});
+
+test('should set amount if valid input provided', () => {
+    const value = '514.70';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe(value);
+});
+test('should not set amount if invalid input provided', () => {
+    const value = '14.7034';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe('');
+});
