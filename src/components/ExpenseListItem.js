61c77b2cd58f23a0d39f8589ead27e73ff4d6e39
hugues Kametcha <hugues.leunsa+atlassian@gmail.com>
2019-11-16 19:08:22 +0100

    Section 13: Deploying your Apps - 138. Formating amount and creation date display

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, note, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}><h3> {description} </h3></Link>
-        <p> Amount : {amount} </p>
-        <p> Date : {createdAt} </p>
+        <p> Amount : {numeral(amount / 100).format('$0,0.00')} </p>
+        <p> Date : {moment(createdAt).format('Do MMMM, YYYY')} </p>
         <p> Note : {note} </p>
     </div>
 );
