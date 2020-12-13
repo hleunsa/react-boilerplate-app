148ee0a6fc48b2c1021f4c3766536a017fe275ae
hugues Kametcha <hugues.leunsa+atlassian@gmail.com>
2020-12-13 02:34:37 +0100

    Section 14 Firebase 101: 142 - Getting Firebase

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import { getVisibleExpenses } from './selectors/expenses';
 
+import "./firebase/firebase";
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
